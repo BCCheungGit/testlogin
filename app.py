b2f554e76c082e99de8706ff12a11002d835834f
BCCheungGit <87406737+BCCheungGit@users.noreply.github.com>
2023-03-31 16:57:05 +0800

    sucessfully learned python login page

--- app.py
+++ app.py
@@ -6,35 +6,69 @@ app = Flask(__name__)
 def validate_user(username, password):
     conn = psycopg2.connect(
         host = "localhost",
-        database = "logindatabase",
+        database = "logindata",
         user="postgres",
         password="root"
     )
     cur = conn.cursor()
-    cur.execute("SELECT username, password FROM logindata WHERE username = %s AND password = %s", (username, password))
+    cur.execute("SELECT username, password FROM logintable WHERE username = %s AND password = %s", (username, password))
     result = cur.fetchone()
+    cur.close()
+    conn.close()
     if result:
         return True
     else:
         return False
 
+def add_user(username, password):
+    conn = psycopg2.connect(
+        host = 'localhost',
+        database = 'logindata',
+        user = 'postgres',
+        password = 'root'
+    )
+    cur = conn.cursor()
+    cur.execute("INSERT INTO logintable(username, password) VALUES (%s, %s)", (username, password))
+    conn.commit()
+    cur.close()
+    conn.close()
+
 @app.route('/')
 def login():
-    return render_template('index.html')
+    return render_template('login.html')
+
+
+@app.route('/signup/')
+def signup():
+    return render_template('signup.html')
 
 @app.route('/login/', methods=['POST', 'GET'])
 def do_login():
-    print('logging in')
-    username = request.form['username']
-    password = request.form['password']
-    if validate_user(username, password):
-        print("Correct password")
-        return render_template('index.html')
-            
-    else:
-        print("incorrect password")
-        return render_template('index.html', error = 'Incorrect Password')
-            
+    if request.method == 'POST':
+        print('logging in')
+        username = request.form['username']
+        password = request.form['password']
+        if validate_user(username, password):
+            print("Correct password")
+            return render_template('dashboard.html')
+                
+        else:
+            print("incorrect password")
+            return render_template('login.html', error = 'Incorrect Password')
 
+@app.route('/dosignup/', methods=['POST', 'GET'])
+def do_signup():
+    if request.method == 'POST':
+        print('signing up')
+        username = request.form['username']
+        password = request.form['password']
+        add_user(username, password)
+        print('added users')
+        return render_template('login.html')           
+
+@app.route('/logout/')
+def log_out():
+    print('logging out')
+    return render_template('login.html')
 if __name__ == '__main__':
     app.run(debug=True)
